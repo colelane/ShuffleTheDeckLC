e6f1194106c7590156a0eba457f825df4423eefc
Lane Coleman <lane.coleman@live.com>
2020-09-18 08:36:26 -0600

    Add project files.

---
+++ ShuffleTheDeckLC.vb
@@ -0,0 +1,42 @@
+﻿Option Strict On
+Option Explicit On
+
+
+
+Module ShuffleTheDeckLC
+
+    Sub Main()
+        Dim min_item As Single
+        Dim max_item As Single
+        Dim i As Single
+        Dim j As Single
+        Dim newVal As Single
+        Dim cards(52) As Integer
+        Dim load As Integer
+
+        For load = 0 To 51
+            cards(load) = load + 1
+            Console.Write(cards(load) & vbTab)
+        Next
+
+        Do
+            min_item = LBound(cards)
+            max_item = UBound(cards)
+
+            Console.WriteLine()
+            Console.ReadLine()
+
+            For i = min_item To max_item - 1
+                ' Randomly assign item number i.
+                j = Int((max_item - i + 1) * Rnd() + i)
+                newVal = cards(CInt(i))
+                cards(CInt(i)) = cards(CInt(j))
+                cards(CInt(j)) = CInt(newVal)
+
+                Console.Write(cards(CInt(i)) & vbTab)
+            Next i
+
+        Loop
+    End Sub
+
+End Module
