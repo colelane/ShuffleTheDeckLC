a36481117a615859283c4bee84d657e792232306
Lane Coleman <lane.coleman@live.com>
2020-09-19 18:07:04 -0600

    added single card dealing

--- ShuffleTheDeckLC.vb
+++ ShuffleTheDeckLC.vb
@@ -1,11 +1,12 @@
 ﻿Option Strict On
 Option Explicit On
-
+Option Compare Text
 
 
 Module ShuffleTheDeckLC
 
     Sub Main()
+        Dim userinput As String
         Dim min_item As Single
         Dim max_item As Single
         Dim i As Single
@@ -13,18 +14,20 @@ Module ShuffleTheDeckLC
         Dim newVal As Single
         Dim cards(52) As Integer
         Dim load As Integer
+        Randomize()
 
+        Console.WriteLine("Press enter to deal a card")
+        Console.ReadLine()
+Shuffle:
         For load = 0 To 51
             cards(load) = load + 1
-            Console.Write(cards(load) & vbTab)
+            'Console.Write(cards(load) & vbTab)
         Next
 
         Do
             min_item = LBound(cards)
             max_item = UBound(cards)
 
-            Console.WriteLine()
-            Console.ReadLine()
 
             For i = min_item To max_item - 1
                 ' Randomly assign item number i.
@@ -33,10 +36,21 @@ Module ShuffleTheDeckLC
                 cards(CInt(i)) = cards(CInt(j))
                 cards(CInt(j)) = CInt(newVal)
 
-                Console.Write(cards(CInt(i)) & vbTab)
+
+                Console.Write(cards(CInt(i)))
+                Console.ReadLine()
             Next i
 
-        Loop
+        Loop Until i = 52
+
+        Console.WriteLine("All cards have been dealt")
+        Console.WriteLine("Press enter to shuffle the deck and start again. Enter Q to quit")
+        userinput = Console.ReadLine()
+        If userinput = "q" Then
+            Exit Sub
+        Else
+            GoTo Shuffle
+        End If
     End Sub
 
 End Module
